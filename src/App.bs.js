4bb3128353ffd8d42532b9dd234d1e9f97794853
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2018-08-24 15:16:59 +1000

    Add function to compute annual tax

--- src/App.bs.js
+++ src/App.bs.js
@@ -3,24 +3,25 @@
 
 var Sys = require("bs-platform/lib/js/sys.js");
 var Caml_array = require("bs-platform/lib/js/caml_array.js");
+var Pervasives = require("bs-platform/lib/js/pervasives.js");
 var Caml_format = require("bs-platform/lib/js/caml_format.js");
 
 var match = Sys.argv.length > 2;
 
-var annualSalary = match ? Caml_format.caml_int_of_string(Caml_array.caml_array_get(Sys.argv, 2)) : 0;
+var annualSalary = match ? Caml_format.caml_float_of_string(Caml_array.caml_array_get(Sys.argv, 2)) : 0.0;
 
-console.log("The annual salary is: " + String(annualSalary));
+console.log("The annual salary is: " + Pervasives.string_of_float(annualSalary));
 
 function calculateTaxBracket(salary) {
-  if (salary <= 18200) {
+  if (salary <= 18200.0) {
     return "Tax Bracket 1";
-  } else if (salary > 18200 && salary <= 37000) {
+  } else if (salary > 18200.0 && salary <= 37000.0) {
     return "Tax Bracket 2";
-  } else if (salary > 37000 && salary <= 90000) {
+  } else if (salary > 37000.0 && salary <= 90000.0) {
     return "Tax Bracket 3";
-  } else if (salary > 90000 && salary <= 180000) {
+  } else if (salary > 90000.0 && salary <= 180000.0) {
     return "Tax Bracket 4";
-  } else if (salary > 180000) {
+  } else if (salary > 180000.0) {
     return "Tax Bracket 5";
   } else {
     return "None";
@@ -29,9 +30,36 @@ function calculateTaxBracket(salary) {
 
 var taxBracket = calculateTaxBracket(annualSalary);
 
-console.log(taxBracket);
+console.log("The tax bracket is: " + taxBracket);
+
+function calculateAnnualTax(salary) {
+  var taxBracket = calculateTaxBracket(salary);
+  var compute = function (nSalary, num1, num2, num3) {
+    return (nSalary - num1) * num2 + num3;
+  };
+  switch (taxBracket) {
+    case "Tax Bracket 1" : 
+        return 0.0;
+    case "Tax Bracket 2" : 
+        return compute(salary, 18200.0, 0.19, 0.0);
+    case "Tax Bracket 3" : 
+        return compute(salary, 37000.0, 0.325, 3572.0);
+    case "Tax Bracket 4" : 
+        return compute(salary, 90000.0, 0.37, 20797.0);
+    case "Tax Bracket 5" : 
+        return compute(salary, 180000.0, 0.45, 54097.0);
+    default:
+      return 0.0;
+  }
+}
+
+var annualTax = calculateAnnualTax(annualSalary);
+
+console.log("The annual tax is: " + Pervasives.string_of_float(annualTax));
 
 exports.annualSalary = annualSalary;
 exports.calculateTaxBracket = calculateTaxBracket;
 exports.taxBracket = taxBracket;
+exports.calculateAnnualTax = calculateAnnualTax;
+exports.annualTax = annualTax;
 /* annualSalary Not a pure module */
--- src/App.re
+++ src/App.re
@@ -1,24 +1,42 @@
 /* Get annual salary from arguments */
-/* let annualSalary = Array.length(Sys.argv) > 2 ? int_of_string(Sys.argv[2]) : 0 */
 let annualSalary = switch(Array.length(Sys.argv) > 2) {
-  | true => int_of_string(Sys.argv[2])
-  | false => 0
+  | true => float_of_string(Sys.argv[2])
+  | false => 0.0
 }
-
-Js.log("The annual salary is: "++string_of_int(annualSalary));
+Js.log("The annual salary is: "++string_of_float(annualSalary))
 
 /* Function to calculate the tax brackeet */
-let calculateTaxBracket = salary => {
-    if(salary <= 18200) { "Tax Bracket 1" }
-    else if(salary > 18200 && salary <= 37000) { "Tax Bracket 2" }
-    else if(salary > 37000 && salary <= 90000) { "Tax Bracket 3" }
-    else if(salary > 90000 && salary <= 180000) { "Tax Bracket 4" }
-    else if(salary > 180000) { "Tax Bracket 5" }
+let calculateTaxBracket = (salary: float) : string => {
+    if(salary <= 18200.0) { "Tax Bracket 1" }
+    else if(salary > 18200.0 && salary <= 37000.0) { "Tax Bracket 2" }
+    else if(salary > 37000.0 && salary <= 90000.0) { "Tax Bracket 3" }
+    else if(salary > 90000.0 && salary <= 180000.0) { "Tax Bracket 4" }
+    else if(salary > 180000.0) { "Tax Bracket 5" }
     else "None"
 }
 
 /* Calculate the tax bracket */
 let taxBracket = calculateTaxBracket(annualSalary)
-Js.log(taxBracket)
+Js.log("The tax bracket is: "++taxBracket)
 
 /* Function to calculate the annual tax */
+let calculateAnnualTax = (salary: float) : float => {
+  /* Calculate the tax bracket */
+  let taxBracket = calculateTaxBracket(salary)
+
+  /* Computing floats require . after their operator */
+  let compute = (nSalary: float, num1: float, num2: float, num3: float) : float => (nSalary -. num1) *. num2 +. num3
+
+  switch(taxBracket) {
+    | "Tax Bracket 1" => 0.0
+    | "Tax Bracket 2" => compute(salary, 18200.0, 0.19, 0.0)
+    | "Tax Bracket 3" => compute(salary, 37000.0, 0.325, 3572.0)
+    | "Tax Bracket 4" => compute(salary, 90000.0, 0.37, 20797.0)
+    | "Tax Bracket 5" => compute(salary, 180000.0, 0.45, 54097.0)
+    | _ => 0.0
+  }
+}
+
+/* Calculate the annual tax */
+let annualTax = calculateAnnualTax(annualSalary)
+Js.log("The annual tax is: "++string_of_float(annualTax))
